4f45125f52fc80986b4f25aca2d41fcb0f53f8c4
danam <dan.am5555555@gmail.com>
2022-01-06 13:08:56 +0200

    javadocs persondata and repos

--- src/main/java/com/Spring/TodoList/TodoListApplication.java
+++ src/main/java/com/Spring/TodoList/TodoListApplication.java
@@ -1,5 +1,7 @@
 package com.Spring.TodoList;
 
+import com.Spring.TodoList.entity.PersonDetails;
+import com.Spring.TodoList.entity.TaskDetails;
 import com.Spring.TodoList.repository.PersonRepository;
 import com.Spring.TodoList.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +9,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @authors: Tal Hazi, Dan Amsalem, Noy Shani
- * @version: 1.0
- * @lastUpdatedDate: Jan 6, 2022
- */
 
 @SpringBootApplication
 public class TodoListApplication implements CommandLineRunner {
 
-	@Autowired
+//	@Autowired
 	private PersonRepository personRepository;
-	@Autowired
+//	@Autowired
 	private TaskRepository taskRepository;
 
 
@@ -26,8 +23,25 @@ public class TodoListApplication implements CommandLineRunner {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
 
-	@Override
+	@Override // where is the exception thrown?
 	public void run(String... args) throws Exception {
 
+//		for (int i = 0; i < 100; i++) {
+//			int finalI = i;
+//			new Thread(() -> {
+//				String name = String.format("Name%d", finalI);
+//				String email = String.format("Email%d@gmail.com", finalI);
+//				String fav = String.format("Java%d", finalI);
+//				PersonDetails p = new PersonDetails(name, email, fav);
+//
+//				String date = "2022-01-05";
+//				String details = String.format("Details%d", finalI);
+//				TaskDetails.Status status = TaskDetails.Status.active;
+//				TaskDetails t = new TaskDetails(name, status, details, date);
+//
+//				personRepository.save(p);
+//				taskRepository.save(t);
+//			}).start();
+//		}
 	}
 }
--- src/main/java/com/Spring/TodoList/repository/PersonRepository.java
+++ src/main/java/com/Spring/TodoList/repository/PersonRepository.java
@@ -3,5 +3,8 @@ package com.Spring.TodoList.repository;
 import com.Spring.TodoList.entity.PersonDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Marker interface for handling the {@link PersonDetails} entity's data
+ */
 public interface PersonRepository extends JpaRepository<PersonDetails,String> {
 }
