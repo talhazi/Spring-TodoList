f15a222196dd6dadc33fb5ff5fc80cdcf185cd92
danam <dan.am5555555@gmail.com>
2022-01-10 20:39:06 +0200

    date javadoc

--- src/main/java/com/Spring/TodoList/request/DateValidator.java
+++ src/main/java/com/Spring/TodoList/request/DateValidator.java
@@ -1,5 +1,9 @@
 package com.Spring.TodoList.request;
 
+/**
+ * Interface for the {@link DateValidatorUsingLocalDate} class, the class that implements the interface has to implement
+ * the method isValid.
+ */
 public interface DateValidator {
     boolean isValid(String dateStr);
 }
--- src/main/java/com/Spring/TodoList/request/DateValidatorUsingLocalDate.java
+++ src/main/java/com/Spring/TodoList/request/DateValidatorUsingLocalDate.java
@@ -3,17 +3,28 @@ package com.Spring.TodoList.request;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
+/**
+ * The class implements the {@link DateValidator} interface and checks that the date given is in the format required.
+ */
 public class DateValidatorUsingLocalDate implements DateValidator {
     private String dateFormat;
 
+    /**
+     * Constructor for the DateValidatorUsingLocalDate class
+     *
+     * @param dateFormatter not null
+     */
     public DateValidatorUsingLocalDate(String dateFormatter) {
         this.dateFormat = dateFormatter;
     }
 
+    /**
+     * The method gets a string and checks if it's in the date format required (according to RSP3339, section 5.6)
+     *
+     * @param dateStr not null
+     * @return boolean not null
+     */
     @Override
     public boolean isValid(String dateStr) {
         DateFormat sdf = new SimpleDateFormat(this.dateFormat);
